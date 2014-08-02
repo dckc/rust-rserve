ad8c8169c2ced411ac7cb3491b474efb387bc2b2
Dan Connolly <dckc@madmode.com>
2014-08-02 13:52:12 -0500

    main program connects and shows protocol  - TODO: move main to integration test?  - set lib crate name to "rserve"  - use u8, since Ascii is only "should"  - rename Other to AnyAttr; add AnyAuth  - refactor early return in decode_id_string

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+#![crate_name = "rserve"]
+#![crate_type = "lib"]
+
 #![feature(phase)]
 #[phase(plugin, link)] extern crate log;
 //extern crate debug;
@@ -5,7 +8,7 @@
 use std::io::net::ip::Port;
 use std::io::{IoResult, IoError, InvalidInput};
 use std::iter::range_inclusive;
-
+use std::str::from_utf8;
 
 pub static DEFAULT_PORT: Port = 6311;
 
@@ -23,59 +26,69 @@ pub enum AttrIndex {
 
 #[deriving(Show, PartialEq, Eq)]
 pub enum ServerAttribute {
-    // TODO: consider keeping the Ascii constraint in the type
-    RVersion(char, char, char),
+    RVersion(u8, u8, u8),
     AuthorizationRequired(AuthType),
-    Key(char, char, char),
-    Other(char, char, char, char)
+    Key(u8, u8, u8),
+    AnyAttr(u8, u8, u8, u8)
 }
 
 #[deriving(Show, PartialEq, Eq)]
 pub enum AuthType {
     PlainText,
     UnixCrypt,
-    MD5
+    MD5,
+    AnyAuth(u8, u8)
 }
 
 pub fn decode_id_string(buf: &[u8]) -> IoResult<ServerProtocol> {
+    debug!("decode_id_string(buf={})", buf);
+
     if buf.len() != 32 {
         return Err(IoError{ kind: InvalidInput, desc: "id string length must be 32", detail: None});
     }
 
-    match buf.to_ascii_opt() {
-        None => Err(IoError{ kind: InvalidInput, desc: "must be ascii", detail: None}),
-        Some(chars) => {
-            // TODO: figure out how to use a pattern here rather than val0, val1
-            for attr_val in [(IDsig, "Rsrv"),
-                             (ServerVersion, "0100"),
-                             (Protocol, "QAP1")].iter() {
-                let attr = (*attr_val).val0();
-                let expected = (*attr_val).val1();
-                let offset = attr as uint * 4;
-                let actual = chars.slice(offset, offset + 4);
-                if actual != expected.to_ascii() {
-                    return Err(IoError{ kind: InvalidInput, desc: "unsupported attribute",
-                                        detail: Some(format!("expected {} for {} but got {}",
-                                                             expected, attr, actual)) })
-                }
-            }
-            let attrs = range_inclusive(Opt4 as uint, Opt8 as uint).map(|ix| {
-                let offset = ix * 4;
-                match (chars[offset].to_char(),
-                       chars[offset + 1].to_char(),
-                       chars[offset + 2].to_char(),
-                       chars[offset + 3].to_char())  {
-                    ('R', c2, c3, c4) => RVersion(c2, c3, c4),
-                    ('A', 'R', 'p', 't') => AuthorizationRequired(PlainText),
-                    ('A', 'R', 'u', 'c') => AuthorizationRequired(UnixCrypt),
-                    ('A', 'R', 'm', '5') => AuthorizationRequired(MD5),
-                    ('K', c2, c3, c4) => Key(c2, c3, c4),
-                    (c1, c2, c3, c4) => Other(c1, c2, c3, c4)
-                }
-            }).collect();
-            Ok(QAP1(attrs))
+
+    let required = [(IDsig, "Rsrv"),
+                    (ServerVersion, "0103"),  // hmm... accept other minor versions?
+                    (Protocol, "QAP1")];
+    let check = |attr: AttrIndex, expected| {
+        let offset = attr as uint * 4;
+        let actual = buf.slice(offset, offset + 4);
+        match from_utf8(actual) {
+            Some(txt) if txt == expected => Ok(()),
+            Some(txt) => Err(IoError{ kind: InvalidInput, desc: "unsupported attribute",
+                                      detail: Some(format!("expected {} for {} but got {}",
+                                                           expected, attr, txt)) }),
+            None => Err(IoError{ kind: InvalidInput, desc: "cannot decode attribute as utf-8",
+                                 detail: Some(format!("bytes: {}", actual)) })
+        }
+    };
+    for attr_val in required.iter() {
+        // TODO: figure out how to use a pattern here rather than val0, val1
+        let attr = (*attr_val).val0();
+        let expected = (*attr_val).val1();
+        match check(attr, expected) {
+            Ok(_) => (),
+            Err(e) => return Err(e)
         }
     }
+
+    let attrs = range_inclusive(Opt4 as uint, Opt8 as uint).map(|ix| {
+        let offset = ix * 4;
+        let (b0, b1, b2, b3) = (buf[offset], buf[offset + 1], buf[offset + 2], buf[offset + 3]);
+        match (b0 as char, b1 as char) {
+            ('R', _) => RVersion(b1, b2, b3),
+            ('A', 'R') => AuthorizationRequired(match (b2 as char, b3 as char) {
+                ('p', 't') => PlainText,
+                ('u', 'c') => UnixCrypt,
+                ('m', '5') => MD5,
+                (_, _) => AnyAuth(b2, b3)
+            }),
+            ('K', _) => Key(b1, b2, b3),
+            (_, _) => AnyAttr(b0, b1, b2, b3)
+        }
+    }).collect();
+    Ok(QAP1(attrs))
 }
 
 pub trait ReadIDString {
@@ -94,7 +107,7 @@ impl<R: Reader> ReadIDString for R {
 #[cfg(test)]
 mod tests {
     use super::{decode_id_string, QAP1,
-                RVersion, AuthorizationRequired, Key, Other,
+                RVersion, AuthorizationRequired, Key, AnyAttr,
                 MD5};
 
     #[test]
@@ -107,12 +120,14 @@ mod tests {
 
     #[test]
     fn ok_id_string() {
-        assert_eq!(decode_id_string("Rsrv0100QAP1****R151ARm5Kabc4444".as_bytes()),
-                   Ok(QAP1(vec!(Other('*', '*', '*', '*'),
-                                RVersion('1', '5', '1'),
+        let actual = decode_id_string("Rsrv0103QAP1****R151ARm5Kabc4444".as_bytes());
+        debug!("protcol: {}", actual);
+        assert_eq!(actual,
+                   Ok(QAP1(vec!(AnyAttr('*' as u8, '*' as u8, '*' as u8, '*' as u8),
+                                RVersion('1' as u8, '5' as u8, '1' as u8),
                                 AuthorizationRequired(MD5),
-                                Key('a', 'b', 'c'),
-                                Other('4', '4', '4', '4')))))
+                                Key('a' as u8, 'b' as u8, 'c' as u8),
+                                AnyAttr('4' as u8, '4' as u8, '4' as u8, '4' as u8)))))
     }
 
     #[test]
